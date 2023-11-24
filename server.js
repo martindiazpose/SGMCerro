ef3c5495bf4c8049288cb333b9672eb03769a8f7
Martin Diaz <martindiazpose@gmail.com>
2023-11-24 15:24:24 -0300

    se configura app.js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const xlsx = require('xlsx');
+const path = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.post('/guardar-en-excel', (req, res) => {
+    const formData = req.body;
+
+    // Lógica para guardar los datos en el archivo Excel
+    const workbook = xlsx.utils.book_new();
+    const worksheet = xlsx.utils.json_to_sheet([formData]);
+    xlsx.utils.book_append_sheet(workbook, worksheet, 'Datos');
+
+    const excelFilePath = path.join(__dirname, 'planilla.xlsx');
+    xlsx.writeFile(workbook, excelFilePath);
+
+    res.send('Datos guardados exitosamente.');
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor iniciado en http://localhost:${PORT}`);
+});
