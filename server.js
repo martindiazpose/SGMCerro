2ac0846f648d55b40e28943199b19cf08a498683
Martin Diaz <martindiazpose@gmail.com>
2023-11-27 14:26:59 -0300

    as

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-require('dotenv').config();
-
 const express = require('express');
 const fetch = require('node-fetch');
 const btoa = require('btoa');
+const dropboxV2Api = require('dropbox-v2-api');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 const APP_KEY = process.env.DROPBOX_APP_KEY;
 const APP_SECRET = process.env.DROPBOX_APP_SECRET;
+const DROPBOX_ACCESS_TOKEN = process.env.DROPBOX_ACCESS_TOKEN; // Puedes obtener esto durante el proceso de autorización
 
 app.get('/auth', (req, res) => {
     const authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${APP_KEY}&response_type=code&redirect_uri=http://localhost:${PORT}/callback`;
@@ -16,28 +16,47 @@ app.get('/auth', (req, res) => {
 });
 
 app.get('/callback', async (req, res) => {
-    const { code } = req.query;
-
-    const tokenUrl = 'https://api.dropbox.com/oauth2/token';
-    const credentials = btoa(`${APP_KEY}:${APP_SECRET}`);
-    const body = `code=${code}&grant_type=authorization_code&redirect_uri=http://localhost:${PORT}/callback`;
-
-    const response = await fetch(tokenUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': `Basic ${credentials}`
-        },
-        body: body
-    });
-
-    const data = await response.json();
-    const accessToken = data.access_token;
-
-    // Puedes almacenar accessToken en tus variables de entorno o base de datos
-    console.log('Access Token:', accessToken);
+    // El código aquí para obtener el token de acceso después de la autorización
+    // ...
 
     res.send('Autenticación exitosa. Puedes cerrar esta ventana.');
+});
+
+app.post('/guardar-en-dropbox', async (req, res) => {
+    try {
+        // Aquí debes implementar la lógica para procesar los datos del formulario
+        const datosFormulario = req.body;
+
+        // Conectar con Dropbox API
+        const dropbox = dropboxV2Api.authenticate({
+            token: DROPBOX_ACCESS_TOKEN,
+        });
+
+        // Nombre del archivo en Dropbox
+        const nombreArchivo = 'formulario.xlsx';
+
+        // Contenido a escribir en el archivo (puedes adaptar esto según la estructura de tu Excel)
+        const contenidoArchivo = `Fecha: ${datosFormulario.fecha}, Hora Comienzo: ${datosFormulario.hora_comienzo}, ...`;
+
+        // Subir el contenido al archivo en Dropbox
+        const response = await dropbox({
+            resource: 'files/upload',
+            parameters: {
+                path: `/${nombreArchivo}`,
+                mode: {
+                    '.tag': 'overwrite',
+                },
+            },
+            readStream: contenidoArchivo,
+        });
+
+        console.log('Archivo guardado en Dropbox:', response);
+
+        res.status(200).send('Datos guardados correctamente en Dropbox');
+    } catch (error) {
+        console.error('Error al guardar en Dropbox:', error);
+        res.status(500).send('Error al procesar la solicitud');
+    }
 });
 
 app.listen(PORT, () => {
