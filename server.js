611be44ea81a8b1c5264e4767481226749af3541
Martin Diaz <martindiazpose@gmail.com>
2023-11-27 14:56:35 -0300

    Eliminar archivo-a-ignorar del repositorio

--- server.js
+++ server.js
@@ -1,64 +1,29 @@
 const express = require('express');
-const fetch = require('node-fetch');
-const btoa = require('btoa');
-const dropboxV2Api = require('dropbox-v2-api');
+const bodyParser = require('body-parser');
+const xlsx = require('xlsx');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const APP_KEY = process.env.DROPBOX_APP_KEY;
-const APP_SECRET = process.env.DROPBOX_APP_SECRET;
-const DROPBOX_ACCESS_TOKEN = process.env.DROPBOX_ACCESS_TOKEN; // Puedes obtener esto durante el proceso de autorización
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-app.get('/auth', (req, res) => {
-    const authUrl = `https://www.dropbox.com/oauth2/authorize?client_id=${APP_KEY}&response_type=code&redirect_uri=http://localhost:${PORT}/callback`;
-    res.redirect(authUrl);
-});
+app.post('/guardar-en-excel', (req, res) => {
+    const formData = req.body;
 
-app.get('/callback', async (req, res) => {
-    // El código aquí para obtener el token de acceso después de la autorización
-    // ...
+    // Lógica para guardar los datos en el archivo Excel
+    const workbook = xlsx.utils.book_new();
+    const worksheet = xlsx.utils.json_to_sheet([formData]);
+    xlsx.utils.book_append_sheet(workbook, worksheet, 'Datos');
 
-    res.send('Autenticación exitosa. Puedes cerrar esta ventana.');
-});
+    const excelFilePath = path.join(__dirname, 'planilla.xlsx');
+    xlsx.writeFile(workbook, excelFilePath);
 
-app.post('/guardar-en-dropbox', async (req, res) => {
-    try {
-        // Aquí debes implementar la lógica para procesar los datos del formulario
-        const datosFormulario = req.body;
-
-        // Conectar con Dropbox API
-        const dropbox = dropboxV2Api.authenticate({
-            token: DROPBOX_ACCESS_TOKEN,
-        });
-
-        // Nombre del archivo en Dropbox
-        const nombreArchivo = 'formulario.xlsx';
-
-        // Contenido a escribir en el archivo (puedes adaptar esto según la estructura de tu Excel)
-        const contenidoArchivo = `Fecha: ${datosFormulario.fecha}, Hora Comienzo: ${datosFormulario.hora_comienzo}, ...`;
-
-        // Subir el contenido al archivo en Dropbox
-        const response = await dropbox({
-            resource: 'files/upload',
-            parameters: {
-                path: `/${nombreArchivo}`,
-                mode: {
-                    '.tag': 'overwrite',
-                },
-            },
-            readStream: contenidoArchivo,
-        });
-
-        console.log('Archivo guardado en Dropbox:', response);
-
-        res.status(200).send('Datos guardados correctamente en Dropbox');
-    } catch (error) {
-        console.error('Error al guardar en Dropbox:', error);
-        res.status(500).send('Error al procesar la solicitud');
-    }
+    res.send('Datos guardados exitosamente.');
 });
 
 app.listen(PORT, () => {
     console.log(`Servidor iniciado en http://localhost:${PORT}`);
 });
+ 
